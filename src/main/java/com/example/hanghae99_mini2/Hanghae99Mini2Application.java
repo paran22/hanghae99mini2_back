b7bab3d1f5a835ca7dbe0d0d94d2ad798ebc8e46
paran21 <knospe1@naver.com>
2022-02-11 17:54:45 +0900

    기본 프로젝트 파일 생성

---
+++ src/main/java/com/example/hanghae99_mini2/Hanghae99Mini2Application.java
@@ -0,0 +1,13 @@
+package com.example.hanghae99_mini2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hanghae99Mini2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hanghae99Mini2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hanghae99_mini2/Hanghae99Mini2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hanghae99_mini2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hanghae99Mini2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

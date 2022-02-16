0456b43090859e287e9cff64b3f74714f6e74288
nerdved <wjdtjsah12@gmail.com>
2022-02-16 23:36:08 +0900

    Timestamped - PreUpdate 내부함수 추가, 오류수정

--- src/main/java/com/example/hanghae99_mini2/domain/Timestamped.java
+++ src/main/java/com/example/hanghae99_mini2/domain/Timestamped.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -18,14 +19,19 @@ public abstract class Timestamped {
     @CreatedDate
     private String createdAt;
 
+//    @LastModifiedDate
+//    private LocalDateTime modifiedAt;
     @LastModifiedDate
-    // LocalDateTime 타입에서 String으로 변경
     private String modifiedAt;
-
 
     @PrePersist
     public void onPrePersist(){
-        this.createdAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
-        this.modifiedAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+        this.createdAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        this.modifiedAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    @PreUpdate
+    public void onPreUpdate() {
+        this.modifiedAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 }
